b356beaa7737be26dedb5c63dd4d8d5acee647a6
aboyneil <neiljoshuamatias03@gmail.com>
2022-08-03 16:00:41 +0800

    first commit

---
+++ config/app.config.js
@@ -0,0 +1,9 @@
+
+const MONGO_DB_CONFIG = {
+    DB: "mongodb+srv://user:test1234@aboycluster.sxrcx.mongodb.net/?retryWrites=true&w=majority"
+};
+
+
+module.exports = {
+    MONGO_DB_CONFIG,
+};
---
+++ controllers/products.controller.js
@@ -0,0 +1,121 @@
+const productServices = require("../services/products.services");
+const upload = require("../middleware/upload");
+
+exports.create = (req, res, next) => {
+    upload(req, res, function(err){
+        if(err){
+            next(err);
+        } else {
+            const url = req.protocol + "://" + req.get("host");
+            const path = req.file != undefined ? req.file.path.replace(/\\/g, "/") : "";
+
+            var model = {
+                productName: req.body.productName,
+                productDescription: req.body.productDescription,
+                productPrice: req.body.productPrice,
+                productImage: path != "" ? url + "/" + path : "",
+            };
+
+            productServices.createProducts(model, (error, results)=>{
+                if(error){
+                    return next(error);
+                } else {
+                    return res.status(200).send({
+                        message: "Success",
+                        data: results
+                    });
+                }
+            });
+        }
+    });
+};
+
+
+exports.findAll = (req, res, next) => {
+            var model = {
+                productName: req.query.productName,
+
+            };
+
+            productServices.getProducts(model, (error, results)=>{
+                if(error){
+                    return next(error);
+                } else {
+                    return res.status(200).send({
+                        message: "Success",
+                        data: results
+                    });
+                }
+            });
+
+};
+
+exports.findOne = (req, res, next) => {
+    var model = {
+        productId: req.params.id,
+
+    };
+
+    productServices.getProductById(model, (error, results)=>{
+        if(error){
+            return next(error);
+        } else {
+            return res.status(200).send({
+                message: "Success",
+                data: results
+            });
+        }
+    });
+
+};
+
+
+exports.update = (req, res, next) => {
+    upload(req, res, function(err){
+        if(err){
+            next(err);
+        } else {
+            const url = req.protocol + "://" + req.get("host");
+            const path = req.file != undefined ? req.file.path.replace(/\\/g, "/") : "";
+
+            var model = {
+                productId: req.params.id,
+                productName: req.body.productName,
+                productDesrcription: req.body.productDesrcription,
+                productPrice: req.body.productPrice,
+                productImage: path != "" ? url + "/" + path : "",
+            };
+
+            productServices.updateProducts(model, (error, results)=>{
+                if(error){
+                    return next(error);
+                } else {
+                    return res.status(200).send({
+                        message: "Success",
+                        data: results
+                    });
+                }
+            });
+        }
+    });
+};
+
+
+exports.delete = (req, res, next) => {   
+    var model = {
+        productId: req.query.id,
+
+    };
+
+    productServices.deleteProducts(model, (error, results)=>{
+        if(error){
+            return next(error);
+        } else {
+            return res.status(200).send({
+                message: "Success",
+                data: results
+            });
+        }
+    });
+
+};
